d959474f391f04d2d0439c67062f2ca9b4594979
Stripmaker24 <33493759+Stripmaker24@users.noreply.github.com>
2022-06-22 12:40:40 +0200

    Firebase set-up

--- firebase-app/src/environments/environment.prod.ts
+++ firebase-app/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'fir-tutorial-69b2c',
+    appId: '1:358653003475:web:b51a66e2508ccba1cf3626',
+    databaseURL: 'https://fir-tutorial-69b2c-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'fir-tutorial-69b2c.appspot.com',
+    apiKey: 'AIzaSyDBBPkyc5_yG-soy2DZgU18bfkqfry4JPo',
+    authDomain: 'fir-tutorial-69b2c.firebaseapp.com',
+    messagingSenderId: '358653003475',
+  },
   production: true
 };
